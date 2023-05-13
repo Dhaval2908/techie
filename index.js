da1a02b412afb96571b1e0990723de9cf57deead
Dhaval2908 <dhavalkhunt17@gmail.com>
2023-05-13 15:34:15 +0530

    path changed for all folder

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const mime = require('mime');
+app.use('/css', express.static(__dirname + '/css', { 
+  setHeaders: function(res, path, stat) {
+    res.set('Content-Type', 'text/css');
+  }
+}));
+app.use('/fonts', express.static(__dirname + '/fonts', {
+
+}));
+app.use('/images', express.static(__dirname + '/images', {
+
+}));
+app.use('/js', express.static(__dirname + '/js', {
+
+}));
+app.use(express.static(path.join(__dirname, 'images')));
 
 // Set up a route for the Coming Soon page
 app.get('/', (req, res) => {
